b0112b2688550d510256ab47d92af6ae6a6a570a
Lukas de Souza Santos <lukas.santos@onclick.com.br>
2023-02-13 11:38:48 -0300

    curso ignite

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,29 @@
+import '../lib/dayjs'
+import { globalStyles } from '@/styles/global'
+import type { AppProps } from 'next/app'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '../lib/react-query'
+import { SessionProvider } from 'next-auth/react'
+import { DefaultSeo } from 'next-seo'
+
+globalStyles()
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            siteName: 'Ignite Call',
+          }}
+        />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
+  )
+}
---
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmStep'
+
+export function ScheduleForm() {
+  const [selectedDateTime, setSelectDateTime] = useState<Date | null>()
+
+  function handleClearSelectedDateTime() {
+    setSelectDateTime(null)
+  }
+
+  if (selectedDateTime) {
+    return (
+      <ConfirmStep
+        schedulingDate={selectedDateTime}
+        onCancelConfirmation={handleClearSelectedDateTime}
+      />
+    )
+  }
+  return <CalendarStep onSelectDateTime={setSelectDateTime} />
+}
